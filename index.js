4ac4b750545ac9c567b4f0154945cb57b5d1c195
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-27 11:48:38 +0300

    feat: add functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+function findMatching(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.toLowerCase().includes(name.toLowerCase());
+  });
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter((driver) => {
+    return driver.toLowerCase().startsWith(letters.toLowerCase());
+  });
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
+
+console.log(matchName(drivers, "Bobby"));
